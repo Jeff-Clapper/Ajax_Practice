3719e770d3890aad34553e29c45ebaa6629fe3a9
Jeff-Clapper <jwclapper@gmail.com>
2021-11-28 10:54:17 -0500

    completed pokemon ajax call

---
+++ Pokemon/javascript.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    for(var ind = 1; ind <= 151; ind++){
+        $(".all_pokemon").append(`<img src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${ind}.png">`)
+    }
+})
