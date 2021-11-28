029424d2a9bd137546f9c17b71400015635fb8de
Jeff-Clapper <jwclapper@gmail.com>
2021-11-28 11:42:01 -0500

    working on my first pokedex assignment

--- Pokemon/javascript.js
+++ Pokemon/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     for(var ind = 1; ind <= 151; ind++){
-        $(".all_pokemon").append(`<img src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${ind}.png">`)
+        $(".all_pokemon").append(`<img src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${ind}.png">`)
     }
 })
